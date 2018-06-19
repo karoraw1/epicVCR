dbd1188c0114ec2619df7211abb4b0cbc6904649
Keith Arora-Williams <login@Keiths-MacBook-Air.local>
2018-06-19 08:46:30 -0400

    per base coverages for cyanos and some analysis

---
+++ scripts/check_cyanos.py
@@ -0,0 +1,3 @@
+import pandas as pd
+fn = "../data/normalized_coverages.txt"
+cyanos = ["NC_006820.1", "NC_031935.1", "NC_025461.1", "NC_013085.1", "NC_031944.1", "NC_025464.1", "NC_031242.1", "NC_025456.1", "NC_031235.1"]
